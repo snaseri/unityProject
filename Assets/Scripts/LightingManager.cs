72476983b6137292bb537125dc808207a3b0bcb4
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2021-09-28 13:29:52 +0100

    added day and night cycle

--- Assets/Scripts/LightingManager.cs
+++ Assets/Scripts/LightingManager.cs
@@ -1,10 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class LightingManager : MonoBehaviour
 {
+    //References
+    [SerializeField ]private Light DirectionalLight;
+    [SerializeField ]private LightingPreset Preset;
+    //Vars
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
     // Start is called before the first frame update
+
+    private void UpdateLighting(float timePrecent)
+    {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePrecent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePrecent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePrecent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePrecent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+
     void Start()
     {
         
@@ -13,6 +57,18 @@ public class LightingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24;
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
     }
 }
--- Assets/Scripts/LightingPreset.cs
+++ Assets/Scripts/LightingPreset.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightingPreset : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+public class LightingPreset : ScriptableObject
+{
+
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+    
+
 }
